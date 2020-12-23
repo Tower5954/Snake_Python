9d1d016e9823c25632d323c23dde7753b4842fc6
Tower59 <g.thompson59@live.co.uk>
2020-12-23 22:56:56 +0000

    new highscore

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+from snake import Snake
+import time
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Tower's Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.15)
+    snake.move()
+
+    #Detect eating food
+    if snake.head.distance(food) < 18:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect biting body
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+        #Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.reset()
+        snake.reset()
+
+
+screen.exitonclick()
+
